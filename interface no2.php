931c31638e201cfc7b2585a20436061b90c8c8bc
AnandaPutriRahma046 <100678860+AnandaPutriRahma046@users.noreply.github.com>
2022-10-19 13:06:00 +0700

    Add files via upload

---
+++ interface no2.php
@@ -0,0 +1,15 @@
+<!-- ANANDA PUTRI RAHMADANI
+     21091397046 -->
+     
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
